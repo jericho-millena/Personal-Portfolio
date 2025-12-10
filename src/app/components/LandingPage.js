dba0090b154e597029b90763199a692ff414375b
jericho-millena <millenajerichojames@gmail.com>
2025-12-11 01:33:20 +0800

    Refactor LandingPage and Profile components for improved readability

--- src/app/components/LandingPage.js
+++ src/app/components/LandingPage.js
@@ -4,17 +4,17 @@ import React, { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 
 export default function LandingPage() {
-  const [fadeOutJohnDoe, setFadeOutJohnDoe] = useState(false);
+  const [fadeOutProfile, setFadeOutProfile] = useState(false);
   const [fadeOutPortfolio, setFadeOutPortfolio] = useState(false);
   const router = useRouter();
 
   useEffect(() => {
-    const johnDoeTimeout = setTimeout(() => setFadeOutJohnDoe(true), 500); // Fade out "John Doe" after 2 seconds
-    const portfolioTimeout = setTimeout(() => setFadeOutPortfolio(true), 2000); // Fade out "Personal Portfolio" after 4 seconds
-    const navigateTimeout = setTimeout(() => router.push("/pages/home"), 2500); // Navigate after 4.5 seconds
+    const profileTimeout = setTimeout(() => setFadeOutProfile(true), 500);
+    const portfolioTimeout = setTimeout(() => setFadeOutPortfolio(true), 2000);
+    const navigateTimeout = setTimeout(() => router.push("/pages/home"), 2500);
 
     return () => {
-      clearTimeout(johnDoeTimeout);
+      clearTimeout(profileTimeout);
       clearTimeout(portfolioTimeout);
       clearTimeout(navigateTimeout);
     };
@@ -22,11 +22,10 @@ export default function LandingPage() {
 
   return (
     <div className="fixed inset-0 flex items-center justify-center ">
-      {/* Inline Text: "John Doe Personal Portfolio" */}
       <h1 className="text-md sm:text-lg md:text-lg lg:text-2xl text-white">
         <span
           className={`transition-opacity duration-500 ${
-            fadeOutJohnDoe ? "opacity-0" : "opacity-100"
+            fadeOutProfile ? "opacity-0" : "opacity-100"
           }`}
         >
           Jericho Millena
